2a827d4fcca2ec50b707833ebac09f9d7ff107dd
Sepideh Shabani <sepidehsh74@gmail.com>
2024-04-18 15:50:33 +0330

    refactor(style): remove vazir font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { DM_Sans, Vazirmatn } from 'next/font/google'
+import { DM_Sans } from 'next/font/google'
 
 import { Sidebar } from '@/components/sidebar/sidebar'
 
@@ -11,20 +11,13 @@ const dm_sans = DM_Sans({
   variable: '--font-dm-sans',
 })
 
-const vazir = Vazirmatn({
-  display: 'swap',
-  subsets: ['arabic'],
-  weight: ['300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-vazir',
-})
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>): JSX.Element {
   return (
-    <html lang="en" className={`${dm_sans.variable} ${vazir.variable}`}>
+    <html lang="en" className={`${dm_sans.variable}`}>
       <body className="min-h-screen flex">
         <Sidebar />
         <main className="w-full">{children}</main>
