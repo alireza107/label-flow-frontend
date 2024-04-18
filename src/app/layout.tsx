a76b87dfde215a4c2015188275bbce6bd48c3798
Sepideh Shabani <sepidehsh74@gmail.com>
2024-04-18 15:49:02 +0330

    feat(style): add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,22 @@
+import { DM_Sans, Vazirmatn } from 'next/font/google'
+
 import { Sidebar } from '@/components/sidebar/sidebar'
 
 import './globals.css'
+
+const dm_sans = DM_Sans({
+  display: 'swap',
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900', '1000'],
+  variable: '--font-dm-sans',
+})
+
+const vazir = Vazirmatn({
+  display: 'swap',
+  subsets: ['arabic'],
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-vazir',
+})
 
 export default function RootLayout({
   children,
@@ -8,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>): JSX.Element {
   return (
-    <html lang="en">
+    <html lang="en" className={`${dm_sans.variable} ${vazir.variable}`}>
       <body className="min-h-screen flex">
         <Sidebar />
         <main className="w-full">{children}</main>
