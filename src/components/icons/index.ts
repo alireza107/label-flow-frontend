e4e02ea86e7ee87775d83cb3979ee93818d544e0
Alireza Dizaji <ardizaji1377@yahoo.com>
2024-04-25 19:05:35 +0330

    feat(comp): add icon creation pipeline with gulp

---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+/* GENERATED FILE */
+
+export { default as IconHome } from './src/home'
+export { default as IconProfile } from './src/profile'
+export { default as IconSetting } from './src/setting'
