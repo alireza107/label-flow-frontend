dd8b2bde924ddf377cdb611d4e0697d66a9ae3e3
Alireza Dizaji <ardizaji1377@yahoo.com>
2024-04-26 13:14:17 +0330

    refactor(comp): show profile icon when src not given in avatar

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import classNames from 'classnames'
 import Image from 'next/image'
 
+import { IconProfile } from '../icons'
 import { Size } from '../types/size.type'
 import { AvatarProps } from './avatar.types'
 
@@ -30,8 +31,7 @@ export const Avatar: React.FC<AvatarProps> = ({
       {src ? (
         <Image src={src} width={sizeClasses[size]} height={sizeClasses[size]} alt={alt} />
       ) : (
-        // TODO: add profile icon after merge icon pack PR
-        <span>avatar not found</span>
+        <IconProfile width={sizeClasses[size] / 2} height={sizeClasses[size] / 2} />
       )}
     </div>
   )
--- src/components/icons/src/home.tsx
+++ src/components/icons/src/home.tsx
@@ -1,7 +1,7 @@
 import BaseIcon from '@/components/icons/base-icon'
 import { SvgIconType } from '@/components/icons/icon.types'
 
-export default function SvgIcon(props: SvgIconType): SvgIconType {
+export default function SvgIcon(props: SvgIconType): JSX.Element {
   return (
     <BaseIcon {...props}>
       <path d="M4 21V9l8-6l8 6v12h-6v-7h-4v7z" />
