66e0da991863c12a549155061f5479711c673976
Sepideh Shabani <sepidehsh74@gmail.com>
2024-05-02 16:51:25 +0330

    feat(comp): header tools

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
 import { Avatar } from '../avatar';
+import '../button';
+import { Button } from '../button';
+import { IconMoon, IconNotifications } from '../icons';
 
 export const Header: React.FC = () => {
   return (
-    <header className="flex items-center border-b border-base-75">
+    <header className="flex items-center border-b border-base-75 py-4">
       <div className="container flex justify-end">
-        <Avatar src="/images/profile.jpg" size="small" />
+        <div className="flex items-center justify-center gap-1 rounded-3xl bg-white p-2">
+          <Button shape="circle">
+            <IconNotifications stroke-width="0.1" fill="currentColor" />
+          </Button>
+          <Button shape="circle">
+            <IconMoon className="base-200" />
+          </Button>
+          <Avatar src="/images/profile.jpg" size="tiny" />
+        </div>
       </div>
     </header>
   );
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,5 +2,7 @@
 
 export { default as IconFavorite } from './src/favorite';
 export { default as IconHome } from './src/home';
+export { default as IconMoon } from './src/moon';
+export { default as IconNotifications } from './src/notifications';
 export { default as IconProfile } from './src/profile';
 export { default as IconSetting } from './src/setting';
