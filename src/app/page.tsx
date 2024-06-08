14bd694a2d7a1cfe5847d433b71ceaa3fccafae3
Alireza Dizaji <ardizaji1377@yahoo.com>
2024-06-08 14:38:24 +0330

    feat(comp): add card skeleton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,16 @@
-import { MultiAvatar } from '@/components/multi-avatar/multi-avatar';
-import { Skeleton } from '@/components/skeleton';
-
-import ProjectCard from './(projects)/_components/project_card';
+import ProjectCard from './(projects)/_components/project-card/project-card';
+import ProjectCardSkeleton from './(projects)/_components/project-card/project-card-skeleton';
 
 export default function Home(): JSX.Element {
   return (
-    <div className="p-10">
+    <div className="flex gap-2 p-10">
       <ProjectCard
         title="Abstract Colors"
         creator="Esthera Jackson"
         image="/images/project.png"
         isFavorite={false}
       />
-
-      <MultiAvatar
-        avatars={[
-          '/images/profile.jpg',
-          '/images/profile.jpg',
-          '/images/profile.jpg',
-          '/images/profile.jpg',
-          '/images/profile.jpg',
-          '/images/profile.jpg',
-        ]}
-        size="small"
-        max={4}
-      />
-      <Skeleton className="bg-error" />
+      <ProjectCardSkeleton />
     </div>
   );
 }
--- src/components/skeleton/skeleton.types.ts
+++
@@ -1,6 +0,0 @@
-export type SkeletonProps = {
-  width?: number;
-  height?: number;
-  className?: string;
-  shape?: 'rect' | 'circle';
-};
